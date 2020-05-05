15815ec324d9ac15f6ee03f641babe00d743623d
Raj5005 <rajmca5005@gmail.com>
2020-05-05 16:29:05 +0530

    framework

---
+++ src/test/java/org/git/hcl/Frameworks.java
@@ -0,0 +1,10 @@
+package org.git.hcl;
+
+public class Frameworks {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" First framework is Data Driven");
+		System.out.println(" Second framework is POM");
+	}
+}
